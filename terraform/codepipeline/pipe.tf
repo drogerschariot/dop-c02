d4ce1aa4714c55ecf98ab25522b07b2a9b36c93a
Drew Rogers <drogers@chariotsolutions.com>
2026-04-08 13:27:14 -0400

    Test

--- terraform/codepipeline/pipe.tf
+++ terraform/codepipeline/pipe.tf
@@ -218,44 +218,41 @@ resource "aws_codepipeline" "codepipeline" {
     }
   }
 
-  stage {
-    name = "Build"
-
-    action {
-      name             = "Build"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      input_artifacts  = ["source_output"]
-      output_artifacts = ["build_output"]
-      version          = "1"
-
-      configuration = {
-        ProjectName = "test"
-      }
-    }
-  }
-
   # stage {
-  #   name = "Deploy"
+  #   name = "Build"
 
   #   action {
-  #     name            = "Deploy"
-  #     category        = "Deploy"
-  #     owner           = "AWS"
-  #     provider        = "CloudFormation"
-  #     input_artifacts = ["build_output"]
-  #     version         = "1"
+  #     name             = "Build"
+  #     category         = "Build"
+  #     owner            = "AWS"
+  #     provider         = "CodeBuild"
+  #     input_artifacts  = ["source_output"]
+  #     output_artifacts = ["build_output"]
+  #     version          = "1"
 
   #     configuration = {
-  #       ActionMode     = "REPLACE_ON_FAILURE"
-  #       Capabilities   = "CAPABILITY_AUTO_EXPAND,CAPABILITY_IAM"
-  #       OutputFileName = "CreateStackOutput.json"
-  #       StackName      = "MyStack"
-  #       TemplatePath   = "build_output::sam-templated.yaml"
+  #       ProjectName = "test"
   #     }
   #   }
   # }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "ElasticBeanstalk"
+      input_artifacts = ["source_output"]
+      version         = "1"
+
+      configuration = {
+        ApplicationName = aws_elastic_beanstalk_application.application.name
+        EnvironmentName = aws_elastic_beanstalk_environment.environment.name
+      }
+    }
+  }
 }
 
 
